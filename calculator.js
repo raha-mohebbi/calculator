eec425180d236ab6ef1b45874454d2e79a3f6905
raha mohebi <rahamhb5@gmail.com>
2025-02-23 23:31:56 +0330

    local storage added to js & css styling is complited

--- calculator.js
+++ calculator.js
@@ -1,10 +1,20 @@
 const display = document.getElementById("display");
-function ondisplay(input){
-display.value +=input;
+function ondisplay(input) {
+display.value += input;
 }
-function cleardisplay(){
+function cleardisplay() {
 display.value = "";
 }
-function calculate(){
-display.value = eval(display.value);
+function calculate() {
+let expr = display.value;
+let result = eval(expr);
+display.value = result;
+let history = JSON.parse(localStorage.getItem("calcHistory") || "[]");
+history.push(expr  + result);
+localStorage.setItem("calcHistory", JSON.stringify(history));
+}
+
+function popup() {
+let history = JSON.parse(localStorage.getItem("calcHistory") || "[]");
+alert(history.length ? history.join("\n") : "no history available");
 }
